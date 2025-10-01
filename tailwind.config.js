28a87ba19fa695ad679982e847493d0ee2ea38f5
Anastasiia Yemelianova <nastia.yemelianova@gmail.com>
2025-10-01 13:50:02 +0300

    fixed fonts and modified file structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{html,js}"],
+  content: ["./index.html", "!./node_modules/**"],
   safelist: [
     "font-clash",
     "font-figtree",
